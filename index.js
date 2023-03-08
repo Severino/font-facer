2e74d8100008731d5e45b0634908e8403e4f975a
Captain Sev <captain.sev@outlook.com>
2023-03-08 09:15:41 +0100

    Fixed 'bin'

--- index.js
+++ index.js
@@ -165,6 +165,8 @@ export default async function main() {
             writeStream.write(fontFace)
         }
     }
+
+    console.log(`Font file was successfully written to: ${outputFileFullPath}.`)
 }
 
 /**
